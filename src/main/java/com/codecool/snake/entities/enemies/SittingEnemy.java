bc7eebbbd4526f0c2613bac814047520417085ae
Panna Kristóf <kristofpanna@gmail.com>
2019-09-19 14:30:26 +0200

    Add not moving enemy

---
+++ src/main/java/com/codecool/snake/entities/enemies/SittingEnemy.java
@@ -0,0 +1,21 @@
+package com.codecool.snake.entities.enemies;
+
+import com.codecool.snake.Globals;
+import com.codecool.snake.entities.Interactable;
+
+public class SittingEnemy extends Enemy implements Interactable {
+    public SittingEnemy() {
+        super(20);
+        setImage(Globals.getInstance().getImage("EnemyPoop"));
+    }
+
+    @Override
+    public void step() {
+        // does not move
+    }
+
+    @Override
+    protected void actionDisappear() {
+        setUpPosition();
+    }
+}
